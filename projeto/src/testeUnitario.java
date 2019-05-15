e08bd1eef549d00fee0886135bd410b038e8145c
raphaCarleto <43914710+raphaCarleto@users.noreply.github.com>
2019-05-14 23:11:20 -0300

    teste

--- projeto/src/testeUnitario.java
+++ projeto/src/testeUnitario.java
@@ -1,8 +1,8 @@
 
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 class testeUnitario {
 
